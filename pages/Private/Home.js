3290df2dc8ee65f234bca44b55e5bfce4c70b1af
joemardev <joemarb.dev@gmail.com>
2022-05-06 14:18:45 +0800

    Update

--- pages/Private/Home.js
+++ pages/Private/Home.js
@@ -11,23 +11,19 @@ const Home = (() => {
     return (
         <>
             <div className=" p-2 w-full">
-                <h2 className="page-title my-5">그래프 판매 통계</h2>
+                <h2 className="page-title my-5">그래프 통계</h2>
 
                 <div className="flex w-full">
                     <Graph title={"회원 입금/출금"} stats={['입금 ', '출금 ', '수익 ']} />
                     <Graph title={"파트너 입금/출금"} stats={['입금 ', '출금 ', '수익 ']} />
                     <Graph title={"카지노"} stats={['WIN ', 'LOSE ', '수익 ']} />
                     <Graph title={"슬롯	"} stats={['WIN ', 'LOSE ', '수익 ']} />
-
-
                 </div>
                 <div className="flex w-full">
                     <Graph title={"호텔카지노	"} stats={['WIN ', 'LOSE ', '수익 ']} />
                     <Graph title={"파워볼	"} stats={['WIN ', 'LOSE ', '수익 ']} />
                     <Graph title={"미니게임"} stats={['WIN ', 'LOSE ', '수익 ']} />
                     <Graph title={"벳이스트	"} stats={['WIN ', 'LOSE ', '수익 ']} />
-
-
                 </div>
             </div>
         </>
